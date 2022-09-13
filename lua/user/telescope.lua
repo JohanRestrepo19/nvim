992978f2585d2dcbc683836b41d48ac45032273d
Johan Restrepo <johanestebanrestrepo@gmail.com>
2022-09-13 16:00:39 -0500

    File browser de telescope añadido

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,71 +1,40 @@
 local status, telescope = pcall(require, "telescope")
 if not status then return end
+
+telescope.load_extension 'file_browser'
+
+
 local actions = require('telescope.actions')
+local builtin = require('telescope.builtin')
+local fb_actions = require "telescope".extensions.file_browser.actions
 
 telescope.setup {
   defaults = {
 
     prompt_prefix = " ",
     selection_caret = " ",
-    path_display = { "smart" },
+    --[[ path_display = { "smart" }, ]]
 
     mappings = {
       n = {
-        ["<esc>"] = actions.close,
+        ["q"] = actions.close,
         ["<CR>"] = actions.select_default,
-
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-
         ["j"] = actions.move_selection_next,
         ["k"] = actions.move_selection_previous,
-        ["H"] = actions.move_to_top,
-        ["M"] = actions.move_to_middle,
-        ["L"] = actions.move_to_bottom,
-
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
         ["gg"] = actions.move_to_top,
         ["G"] = actions.move_to_bottom,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-
         ["?"] = actions.which_key,
       },
 
       i = {
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
 
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
-
         ["<C-c>"] = actions.close,
-
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-
         ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+        ["<Tab>"] = actions.move_selection_worse,
+        ["<S-Tab>"] = actions.move_selection_better,
         ["<C-l>"] = actions.complete_tag,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
       },
 
     },
@@ -88,6 +57,20 @@ telescope.setup {
     --   extension_config_key = value,
     -- }
     -- please take a look at the readme of the extension you want to configure
+    file_browser = {
+      theme        = "dropdown",
+      hijack_netrw = true,
+      mappings     = {
+        ['i'] = {
+          -- your custom insert mode mappings
+        },
+        ['n'] = {
+          -- your custom normal mode mappings
+          ['N'] = fb_actions.create,
+          ['h'] = fb_actions.goto_parent_dir,
+        }
+      }
+    }
   },
 }
 
@@ -95,3 +78,16 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<c-p>", ":Telescope find_files<cr>", opts)
 vim.keymap.set("n", "<leader>ft", ":Telescope live_grep<cr>", opts)
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<cr>", opts)
+--[[ vim.keymap.set("n", "<leader>e", ":Telescope file_browser<cr>", opts) ]]
+
+--[[ vim.keymap.set("n", "<leader>e", function() ]]
+--[[   telescope.extensions.file_browser.file_browser({ ]]
+--[[     path = "%:p:h", -- start from within current buffer path ]]
+--[[     respect_gitignore = false, ]]
+--[[     hidden = true, ]]
+--[[     grouped = true, ]]
+--[[     previewer = false, ]]
+--[[     initial_mode = "normal", ]]
+--[[     layout_config = { height = 40 } ]]
+--[[   }) ]]
+--[[ end, opts) ]]
