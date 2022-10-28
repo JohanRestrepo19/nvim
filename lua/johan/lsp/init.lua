4498ae8d41c9893c170ed20203f87ba791ecef55
Johan Restrepo <johanestebanrestrepo@gmail.com>
2022-10-28 13:59:08 -0500

    Cambio a la carpta user.

--- lua/johan/lsp/init.lua
+++
@@ -1,3 +0,0 @@
-require("johan.lsp.lsp-config")
-require("johan.lsp.lsp-saga")
-require("johan.lsp.null-ls")
---
+++ lua/nighteye/lsp/init.lua
@@ -0,0 +1,3 @@
+require("nighteye.lsp.lsp-config")
+require("nighteye.lsp.lsp-saga")
+require("nighteye.lsp.null-ls")
