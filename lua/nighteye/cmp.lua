104811027f7764e00ce6002ddfaa4a61deb4c89e
Johan Restrepo <johanestebanrestrepo@gmail.com>
2022-11-09 20:41:29 -0500

    Limpieza de plugins.
    Se eliminaron los snippets personalizados ya que no se les estaba dando
    uso.

--- lua/nighteye/cmp.lua
+++ lua/nighteye/cmp.lua
@@ -8,16 +8,8 @@ if not snip_status_ok then
   return
 end
 
--- Load custom snippets
-require("luasnip/loaders/from_vscode").lazy_load({ paths = './lua/user/my-snippets' })
-
 -- Load default snippets
 require("luasnip/loaders/from_vscode").lazy_load()
-
-local check_backspace = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-end
 
 cmp.setup {
   snippet = {
@@ -47,34 +39,6 @@ cmp.setup {
       select = true
     },
 
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
   }),
 
   sources = cmp.config.sources({
--- lua/nighteye/lsp/lsp-saga.lua
+++ lua/nighteye/lsp/lsp-saga.lua
@@ -13,4 +13,4 @@ keymap('n', 'K', '<Cmd>Lspsaga hover_doc<cr>', opts)
 keymap('n', '<leader>ca', '<Cmd>Lspsaga code_action<cr>', opts)
 --[[ vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<cr>', opts) ]]
 keymap('n', 'gp', '<Cmd>Lspsaga preview_definition<cr>', opts)
-keymap('n', 'grr', '<Cmd>Lspsaga rename<cr>', opts)
+keymap('n', 'gr', '<Cmd>Lspsaga rename<cr>', opts)
