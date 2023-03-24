186e259f17e3f809149b8dfbe94cd2364020fa10
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-03-24 00:15:11 -0500

    feat(plugins): nvim-tree centered floating window.

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -4,6 +4,8 @@ if not status then return end
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
 null_ls.setup({
   sources = {
     -- Formatters
