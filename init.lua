755e306a398c7930c8c3ad9f765d20b6ec439b5b
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-04-17 08:52:52 -0500

    feat: add wsl clipboard support.

--- init.lua
+++ init.lua
@@ -4,3 +4,7 @@ require 'nighteye.maps'
 require 'nighteye.autocommands'
 require 'nighteye.plugins'
 require 'nighteye.colorschemes.gruvbox'
+
+local has = vim.fn.has
+local is_wsl = has "wsl"
+if is_wsl then require 'nighteye.wsl' end
