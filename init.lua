6f336d4bd024e6d9d70e606519160d163087914b
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-06-12 22:01:57 -0500

    feat(ui): add pum trasnparency.

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@
 
 require 'nighteye.options'
 require 'nighteye.base'
+require 'nighteye.highlights'
 require 'nighteye.maps'
 require 'nighteye.autocommands'
 require 'nighteye.plugins'
--- lua/nighteye/colorschemes/kanagawa.lua
+++ lua/nighteye/colorschemes/kanagawa.lua
@@ -30,7 +30,11 @@ kanagawa.setup({
   overrides = function(colors) -- add/modify highlights
     local theme = colors.theme
     return {
-      Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+      Pmenu = {
+        fg = theme.ui.shade0,
+        bg = theme.ui.bg_p1,
+        blend = vim.o.pumblend
+      }, -- add `blend = vim.o.pumblend` to enable transparency
       PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
       PmenuSbar = { bg = theme.ui.bg_m1 },
       PmenuThumb = { bg = theme.ui.bg_p2 },
