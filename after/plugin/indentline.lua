03097a1e0d63bbf55197965179581f1cd2769754
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-09-28 11:47:25 -0500

    feat(plugins): update IndentBlankLine config.

--- after/plugin/indentline.lua
+++ after/plugin/indentline.lua
@@ -1,13 +1,10 @@
-local status, blank_line = pcall(require, 'indent_blankline')
+local status, blank_line = pcall(require, 'ibl')
 if not status then return end
 
-blank_line.setup({
-  enabled = false,
-  use_treesitter = true
-})
+blank_line.setup({ enabled = false })
 
 local function opts(desc)
   return { desc = 'IndentBlankline: ' .. desc, noremap = true, silent = true, nowait = true }
 end
 
-vim.keymap.set('n', '<leader>ti', ':IndentBlanklineToggle<cr>', opts('Toggle indent lines'))
+vim.keymap.set('n', '<leader>ti', ':IBLToggle<cr>', opts('Toggle indent lines'))
