a7a65404bbd10147220c2c77c24d4ef236f98496
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-10-02 08:35:48 -0500

    chore: update todo list.

--- init.lua
+++ init.lua
@@ -7,7 +7,6 @@
 
 -- TODO:
 -- [ ] Customize bufferline to like vim airline.
--- [ ] Change lsp format capabilites to its own shorcut and not when saving a file.
 
 -- Plugins
 -- [x] treesitter
@@ -21,8 +20,8 @@
 -- [x] cmp
 -- [x] bufferline (or equivalent)
 -- [x] lualine (or equivalent) 
+-- [x] lsp (and all its parts)
 
--- [ ] lsp (and all its parts)
 -- [ ] null-ls (or equivalent)
 
 
