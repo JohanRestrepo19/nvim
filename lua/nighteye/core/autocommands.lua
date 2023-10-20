ca79a88d8b4aad859240cbfcb1245ed34dad8e4b
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-10-20 10:48:33 -0500

    feat: add autowrap for markdown files.

--- lua/nighteye/core/autocommands.lua
+++ lua/nighteye/core/autocommands.lua
@@ -1,6 +1,14 @@
 -- Fixes Autocomment in newlines
 vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
   callback = function()
-    vim.cmd "set formatoptions-=cro"
+    vim.cmd('set formatoptions-=cro')
   end,
+})
+
+-- Set wrap for markdown files
+vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+  pattern = { '*.md', '*.mdx' },
+  callback = function()
+    vim.cmd('set wrap')
+  end
 })
