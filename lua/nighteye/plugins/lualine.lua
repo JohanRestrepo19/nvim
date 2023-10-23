dceff62777b7b89c0ee70f87282cc7682dddf51e
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-10-22 19:11:52 -0500

    feat(colorschemes): add neosolarized theme.

--- lua/nighteye/plugins/lualine.lua
+++ lua/nighteye/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
     lualine.setup({
       options = {
         icons_enabled = true,
-        theme = "auto",
+        theme = "solarized_dark",
         globalstatus = false,
         -- component_separators = { left = "", right = "" },
         -- section_separators = { left = "", right = "" },
