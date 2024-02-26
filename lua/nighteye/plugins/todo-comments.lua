c82db5892974a7540ae956aefbabb00e871ec052
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-02-26 10:47:03 -0500

    chore: update todo-comments keymaps.

--- lua/nighteye/plugins/todo-comments.lua
+++ lua/nighteye/plugins/todo-comments.lua
@@ -1,5 +1,12 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
+    config = function()
+        local todo_comments = require("todo-comments")
+        local opts = { noremap = true, silent = true }
+
+        todo_comments.setup({})
+
+        vim.keymap.set("n", "<leader>tl", ":TodoLocList<cr>", opts)
+    end,
 }
